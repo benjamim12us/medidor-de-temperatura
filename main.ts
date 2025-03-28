4273569f1d715bb9469c9d787b94b56d3b4fff3d
benjamim12us <00001149604311sp@al.educacao.sp.gov.br>
2025-03-28 09:50:11 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 basic.forever(function () {
     if (input.temperature() > 25) {
         music.play(music.stringPlayable("C5 B C5 B B B C5 C5 ", 999), music.PlaybackMode.UntilDone)
-        basic.showNumber(0)
+        basic.showNumber(input.temperature())
+        basic.showArrow(ArrowNames.North)
+        basic.clearScreen()
+    } else if (input.temperature() < 22) {
+        music.play(music.stringPlayable("C5 G A G F B G G ", 999), music.PlaybackMode.UntilDone)
+        basic.showNumber(input.temperature())
+        basic.showArrow(ArrowNames.South)
+        basic.clearScreen()
     } else {
-    	
+        basic.showIcon(IconNames.Yes)
+        basic.clearScreen()
+        basic.showNumber(input.temperature())
     }
 })
